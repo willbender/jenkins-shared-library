16e6a4cecb2dbab6e43539a7813fcc3071d2ea77
William Bernal <william.bernal@munoz-galeano.com>
2025-03-27 12:09:20 -0500

    Refactor environment variable definitions and update stage name in Maven build scripts

--- vars/mavenBuildArtefact.groovy
+++ vars/mavenBuildArtefact.groovy
@@ -1,7 +1,7 @@
 //Build/Compile an artifact with maven
 def call(Map config){
-    stage("Build Artefict"){
-        docker.image("maven:${env.MVN_TAG}"){
+    stage("Build Artifact"){
+        docker.image("maven:${env.MVN_TAG}").inside(){
             // See if the project can be compiled.
             echo "Compiling ${env.BRANCH_NAME} Branch"
             sh "${MVN_CMD} -U clean compile"
--- vars/mavenDefineEnvironment.groovy
+++ vars/mavenDefineEnvironment.groovy
@@ -1,21 +1,21 @@
 def call(Map config){
     //Environment variables definition
     //Name of the branch where the pipeline is running
-    env.BRANCH_NAME = getGitBranchName()
+    def env.BRANCH_NAME = getGitBranchName()
     //Maven command to use
-    env.MVN_CMD = 'mvn -ntp '
+    def env.MVN_CMD = 'mvn -ntp '
     //Docker image for maven to use for maven steps.
-	env.MVN_TAG = "${config.mvnVersion}"
+	def env.MVN_TAG = "${config.mvnVersion}"
     
     //Docker registry for images.
-    env.DOCKER_REGISTRY = "${config.dockerRegistry}"
+    def env.DOCKER_REGISTRY = "${config.dockerRegistry}"
     //Some information is read from pom file.
-    pom = readMavenPom file: 'pom.xml'
-    env.GROUP_ID = pom.groupId
-    env.ARTIFACT_ID = pom.artifactId
-    env.POM_VERSION = pom.version
-    env.IMAGE_NAME = getPomImageName()
-    env.IMAGE_VERSION = getPomImageVersion(env.BRANCH_NAME)
+    def pom = readMavenPom file: 'pom.xml'
+    def env.GROUP_ID = pom.groupId
+    def env.ARTIFACT_ID = pom.artifactId
+    def env.POM_VERSION = pom.version
+    def env.IMAGE_NAME = getPomImageName()
+    def env.IMAGE_VERSION = getPomImageVersion(env.BRANCH_NAME)
     //Changes the display of the build in Jenkins
     currentBuild.displayName = env.IMAGE_VERSION
     currentBuild.description = env.IMAGE_NAME + ' - ' + env.IMAGE_VERSION
